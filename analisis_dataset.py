b7a4ba1bc5bac00d1e96eb35228a2a8055878dc3
claudiaalozano <clau.lozano.perez@gmail.com>
2022-04-28 10:17:30 +0200

    creo otra función para mostrar un gráfico por si el otro no vale

--- analisis_dataset.py
+++ analisis_dataset.py
@@ -22,7 +22,7 @@ def añadir_columna():
     df.to_csv('Pokemon_modificado.csv', index=False)
 añadir_columna()
 
-#función que analice Pokemon.csv y nos devuelva una serie con los datos de cada pokemon
+
 def analisis_dataset():
     df = pd.read_csv('Pokemon.csv')
     df = df.drop(['#'], axis=1)
@@ -40,10 +40,6 @@ def analisis_dataset():
 analisis_dataset()
 
 
-
-
-
-#función que te calcule el 68%, el 95% y el 97% de cada columna de Pokemon.csv
 def percentiles():
     df = pd.read_csv('Pokemon.csv')
     df = df.drop(['#'], axis=1)
@@ -62,7 +58,7 @@ def percentiles():
     df.describe(percentiles=[.68, .95, .97])
 percentiles()
 
-# funcion que haga la media de la columna Total de Pokemon.csv
+
 def media_total():
     df = pd.read_csv('Pokemon.csv')
     df = df.drop(['#'], axis=1)
@@ -79,7 +75,7 @@ def media_total():
     df['Total'].mean()
 media_total()
 
-#función que calcule la mediana y la moda de la columna Total de Pokemon.csv
+
 def mediana_moda_total():
     df = pd.read_csv('Pokemon.csv')
     df = df.drop(['#'], axis=1)
@@ -97,7 +93,7 @@ def mediana_moda_total():
     df['Total'].mode()
 mediana_moda_total()
 
-#función que calcule la varianza y la desviación estándar de Pokemon.csv
+
 def varianza_desviacion_estandar():
     df = pd.read_csv('Pokemon.csv')
     df = df.drop(['#'], axis=1)
@@ -117,5 +113,22 @@ def varianza_desviacion_estandar():
 
 sample_data = random.normal("media", "desviacion", size=1000)
 plt.hist(sample_data)
+plt.show()
+
+#mostrar gráfico de barras de Pokemon.csv
+df = pd.read_csv('Pokemon.csv')
+df = df.drop(['#'], axis=1)
+df = df.drop(['Type 2'], axis=1)
+df = df.drop(['Generation'], axis=1)
+df = df.drop(['Legendary'], axis=1)
+df = df.drop(['Name'], axis=1)
+df = df.drop(['Total'], axis=1)
+df = df.drop(['HP'], axis=1)
+df = df.drop(['Attack'], axis=1)
+df = df.drop(['Defense'], axis=1)
+df = df.drop(['Sp. Atk'], axis=1)
+df = df.drop(['Sp. Def'], axis=1)
+df = df.drop(['Speed'], axis=1)
+df.plot(kind='bar')
 plt.show()
 
